d72f8826c0586493aa1267a5ec4bbc0b32b15cbc
Jason Charnes <jason@thecharnes.com>
2020-12-11 12:03:39 -0600

    Refactors to Stimulus 2.0 class and value API

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -2,7 +2,16 @@ import { Controller } from "stimulus";
 import StimulusReflex from "stimulus_reflex";
 
 export default class extends Controller {
+  static classes = ["complete"];
+  static targets = ["checkbox", "label"];
+  static values = { complete: Boolean };
+
   connect() {
     StimulusReflex.register(this);
+
+    if (this.completeValue) {
+      this.checkboxTarget.checked = true;
+      this.labelTarget.classList.add(this.completeClass);
+    }
   }
 }
