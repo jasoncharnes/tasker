ddbc1b139b14593dfc08ee7aa0f8de5c0a70ee04
Jason Charnes <jason@thecharnes.com>
2020-12-13 15:34:38 -0600

    Adds before_reflex for finding task

--- app/reflexes/task_reflex.rb
+++ app/reflexes/task_reflex.rb
@@ -1,9 +1,17 @@
 class TaskReflex < StimulusReflex::Reflex
+  before_reflex :find_task
+
   def toggle
-    Task.find(element.dataset.id).update(completed_at: element.checked ? Time.current : nil)
+    @task.update(completed_at: element.checked ? Time.current : nil)
   end
 
   def destroy
-    Task.find(element.dataset.id).destroy
+    @task.destroy
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(element.dataset.id)
   end
 end
