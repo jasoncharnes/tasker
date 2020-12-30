812427c3bb50fba257bb4cbd2f253ddbdccb6677
Jason Charnes <jason@thecharnes.com>
2020-12-30 11:09:06 -0600

    Refactors DOM for editing tasks

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -2,8 +2,18 @@ import { Controller } from "stimulus";
 import StimulusReflex from "stimulus_reflex";
 
 export default class extends Controller {
+  static targets = ["checkbox", "collapse"];
+
   connect() {
     StimulusReflex.register(this);
+
+    $(this.collapseTarget).on("show.bs.collapse", () => {
+      this.checkboxTarget.classList.add("d-none");
+    });
+
+    $(this.collapseTarget).on("hide.bs.collapse", () => {
+      this.checkboxTarget.classList.remove("d-none");
+    });
   }
 
   destroy(event) {
