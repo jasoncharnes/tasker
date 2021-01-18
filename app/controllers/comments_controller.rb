ef21622e0c2a56b396e836d826dc5a1170c9bccd
Jason Charnes <jason@thecharnes.com>
2021-01-18 17:14:14 -0600

    Clears comment form after submission

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,10 @@ class CommentsController < ApplicationController
         selector: "#comments",
         html: render_to_string(@comment)
       )
+      cable_ready[TaskChannel].outer_html(
+        selector: "#new-comment",
+        html: render_to_string(partial: "comments/form", locals: { new_comment: Comment.new })
+      )
     end
 
     cable_ready.broadcast_to(@task)
