be51423314f645c7138e06c1bec68f4a36646dc6
Jason Charnes <jason@thecharnes.com>
2021-01-31 17:44:25 -0600

    Live in-completes™ tasks

--- app/reflexes/task_reflex.rb
+++ app/reflexes/task_reflex.rb
@@ -4,18 +4,18 @@ class TaskReflex < StimulusReflex::Reflex
   def toggle
     if element.checked
       @task.update(completed_at: Time.current, completer: connection.current_user)
-
-      cable_ready[ListChannel]
-        .remove(selector: "#task_#{@task.id}")
-        .insert_adjacent_html(
-          selector: "#list_#{@task.list_id} #complete-tasks",
-          position: "beforeend",
-          html: ApplicationController.render(@task)
-        )
-        .broadcast_to(@task.list)
     else
       @task.update(completed_at: nil, completer: nil)
     end
+
+    cable_ready[ListChannel]
+      .remove(selector: "#task_#{@task.id}")
+      .insert_adjacent_html(
+        selector: "#list_#{@task.list_id} #{element.checked ? '#complete-tasks' : '#incomplete-tasks'}",
+        position: "beforeend",
+        html: ApplicationController.render(@task)
+      )
+      .broadcast_to(@task.list)
   end
 
   def destroy
