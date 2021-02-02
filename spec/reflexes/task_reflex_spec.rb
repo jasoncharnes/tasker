e496c4e4bccfa803576ea9446482eca8c56bf08c
Jason Charnes <jason@thecharnes.com>
2021-02-02 09:31:19 -0600

    Tests CableReady broadcasts

--- spec/reflexes/task_reflex_spec.rb
+++ spec/reflexes/task_reflex_spec.rb
@@ -10,20 +10,50 @@ RSpec.describe TaskReflex, type: :reflex do
     context "element is checked" do
       let(:task) { FactoryBot.create(:task) }
 
-      it "completes the task" do
+      before do
         reflex.element.dataset.id = task.id
         reflex.element.checked = true
+      end
+
+      it "completes the task" do
         expect { subject }.to change { task.reload.complete? }.from(false).to(true)
+      end
+
+      it "broadcasts CableReady operations" do
+        expect { subject }.to broadcast(
+          remove: { selector: "#task_#{task.id}" },
+          insert_adjacent_html: {
+            selector: "#list_#{task.list_id} #complete-tasks",
+            position: "beforeend",
+            html: kind_of(String)
+          },
+          broadcast_to: task.list
+        )
       end
     end
 
     context "element is unchecked" do
       let(:task) { FactoryBot.create(:task, :complete) }
 
-      it "incompletes the task" do
+      before do
         reflex.element.dataset.id = task.id
         reflex.element.checked = false
+      end
+
+      it "incompletes the task" do
         expect { subject }.to change { task.reload.complete? }.from(true).to(false)
+      end
+
+      it "broadcasts CableReady operations" do
+        expect { subject }.to broadcast(
+          remove: { selector: "#task_#{task.id}" },
+          insert_adjacent_html: {
+            selector: "#list_#{task.list_id} #incomplete-tasks",
+            position: "beforeend",
+            html: kind_of(String)
+          },
+          broadcast_to: task.list
+        )
       end
     end
   end
